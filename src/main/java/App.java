80e1182ac50086e40520ad7669183d3e4d33ec61
Blake Womack <lakebayomackway@gmail.com>
2017-03-20 09:10:14 -0700

    add template form.vtl.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,6 +16,12 @@ public class App {
       return new ModelAndView(new HashMap(), "templates/favorite_photos.vtl");
     }, new VelocityTemplateEngine());
 
+    get("/form", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("template", "templates/form.vtl");
+    return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 // public class App {
 //   public static void main(String[] args) {
 //     staticFileLocation("/public");
