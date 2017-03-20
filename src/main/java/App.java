7bc4766cf682ff5a10718d503800eb34549b1373
Blake Womack <lakebayomackway@gmail.com>
2017-03-20 10:33:14 -0700

    add greeting_card.vtl.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,6 +27,16 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/greeting_card", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String recipient = request.queryParams("recipient");
+      String sender = request.queryParams("sender");
+      model.put("recipient", recipient);
+      model.put("sender", sender);
+      model.put("template", "templates/greeting_card.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 // public class App {
 //   public static void main(String[] args) {
 //     staticFileLocation("/public");
