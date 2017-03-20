77af41cfd3a19507607712351807fee772200d26
likewater <lakebayomackway@gmail.com>
2017-03-19 22:53:41 -0700

    add link from root to pictures page.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,22 +3,23 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
-    get("/hello", (request, response) ->
+    get("/", (request, response) ->
         "<!DOCTYPE html>" +
          "<html>" +
          "<head>" +
-           "<title>Hello Friend!</title>" +
+           "<title>Bonjour Friend!</title>" +
            "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
          "</head>" +
         "<body>" +
            "<h1>Hello From Afar</h1>" +
            "<p>Dear Friend,</p>" +
-           "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
+           "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in Switzerland while I learn programming! </p>" +
            "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
            "<p>But I like programming a lot, so I've got that going for me. </p>" +
            "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
            "<p>Cheers,</p>" +
-           "<p>Travel Enthusiast Jane</p>" +
+           "<p>Travel Enthusiast Blake</p>" +
+           "<p><a href='/favorite_photos' >P.S. Check out my favorite travel photos here.</a></p>" +
          "</body>" +
        "</html>"
     );
@@ -34,7 +35,7 @@ public class App {
        "<h1>Favorite Traveling Photos</h1>" +
           "<ul>" +
             "<li><img src='/images/eila-lifflander-220723.jpg' alt='A photo of a mountain.'/></li>" +
-            "<li><img src='/images/joshua-earle-35836.jpg' alt='A photo of a a rocky beach.'/></li>" +
+            "<li><img src='/images/joshua-earle-35836.jpg' alt='A photo of a mountain.'/></li>" +
           "</ul>" +
       "</body>" +
       "</html>"
